34989a30e94ce92730e31700a24e33014dcdbc6c
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2016-11-22 22:42:36 -0500

    more replacements

--- cogs/general.py
+++ cogs/general.py
@@ -25,12 +25,17 @@ class General:
     await self.bot.say("Pong.")
 
   async def respond(self, message):
+    if message.author.bot:
+      return
+
     for i in self.conf['responses']:
       if re.search("(?i){}".format(i[0]), message.content):
         rep = i[1]
-        subs = {"\\{un\\}":message.author.name,
-                "\\{um\\}":message.author.mention,
-                "\\{ui\\}":message.author.id}
+        subs = {"\\{un\\}"         : message.author.name,
+                "\\{um\\}"         : message.author.mention,
+                "\\{ui\\}"         : message.author.id,
+                "\\{situations\\}" : random.choice(self.conf['situations'])
+               }
         for j in re.findall("\\(.*\\|.*\\)", rep):
           rep = rep.replace(j, random.choice(j[1:-1].split("|")))
         for j in subs:

9791506308e5c61a7c75b541f2c9287ce68c2827
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2016-12-16 14:17:35 -0500

    added lenny face, and a few img updates
    - `-term` will ensure that `term` does not appear in the match
    - if a zip or a cbz file is matched, it will be extracted
       and the contentes puushed
    - comments added
      - python style `#`
      - c style `//` and `/* ... */`

--- cogs/az.py
+++ cogs/az.py
@@ -9,7 +9,7 @@ import os
 from discord.ext import commands
 from .utils.format import *
 from .utils import perms
-from .utils.find import find
+from .utils import find as azfind
 from .utils.config import Config
 
 class AZ:
@@ -27,45 +27,91 @@ class AZ:
       self.conf['images'] = {}
 
   @commands.command()
+  async def lenny(self):
+    await self.bot.say('( ͡° ͜ʖ ͡° )');
+
+  @commands.command()
   @perms.in_group('img')
   async def img(self, *search):
     if 'path' not in self.conf or not os.path.exists(self.conf['path']):
       await self.bot.say('path does not exist')
 
-    search = [re.sub('[^\\w\\.-]+', '', i).lower() for i in search]
+    search = [re.sub(r'[^\w\./#\*-]+', '', i).lower() for i in search]
+    for i in range(len(search)):
+      if re.search('^(//|#)', search[i]):
+        search = search[:i]
+        break
+
+    for i in range(len(search)):
+      if re.search('^(/\\*)', search[i]):
+        for j in range(i, len(search)):
+          if re.search('^(\\*/)', search[j]):
+            break
+        search = search[:i] + search[j+1:]
+        break
 
     try:
-      path = find(self.conf['path'], '*'.join(search))
+      path = azfind.search(self.conf['path'], search)
     except:
+      raise
       path = ''
 
+    if not path:
+      await self.bot.say("couldn't find anything matching: `{}`".format(search))
+      return
+
+    await self.bot.say(self.get_url(path))
+
+  def confirm_img(self, iids):
+    for iid in iids.split('\n'):
+      if not self.account.thumbnail(iid):
+        return False
+    return True
+
+  def get_url(self, path):
     if path in self.conf['images']:
       image_id = self.conf['images'][path]['id']
-      url = self.conf['images'][path]['url']
+      url      = self.conf['images'][path]['url']
       try:
-        if self.account.thumbnail(image_id):
-          await self.bot.say(url)
-          return
+        if confirm_img(image_id):
+          return url
       except:
         pass
 
     out = ''
-    if path:
+    if path.rpartition('.')[2].lower() in ['zip', 'cbz']:
+      files = azfind.extract(path)
+      if files:
+        out = self.upload(files, path)
+        for f in files:
+          os.remove(f)
+        os.rmdir(os.path.dirname(files[0]))
+      else:
+        out = 'archive found... but empty'
+      return out
+    else:
+      return self.upload([path])
+
+  def upload(self, paths, p=None):
+    if not p:
+      p = paths[0]
+    iids = ''
+    urls = ''
+    for path in paths:
       for i in range(3):
         try:
           image = self.account.upload(path)
           if image and image.url:
-            self.conf['images'][path] = {'id':image.id, 'url':image.url}
-            self.conf.save()
-            out = image.url
+            iids += image.id  + '\n'
+            urls += image.url + '\n'
             break
         except ValueError:
           pass
       else:
-        out = 'could not upload image'
-    else:
-      out = 'could not find anything matching: `{}`'.format(search)
-    await self.bot.say(out)
+        return 'could not upload image'
+    self.conf['images'][p] = {'id':iids, 'url':urls}
+    self.conf.save()
+    return urls
 
 def setup(bot):
   bot.add_cog(AZ(bot))
--- cogs/utils/find.py
+++ cogs/utils/find.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 
-import fnmatch
+import zipfile
+import tempfile
 import random
 import os
 
-def find(directory, pattern, single=True):
-  pattern = '*'+pattern.replace(' ', '*')+'*'
+def search(directory, pattern, single=True):
+  pattern = set(filter(None, pattern))
   matches = []
-  
+
   for root, directories, filenames in os.walk(directory):
     for filename in filenames:
       filename = os.path.join(root,filename)
       filename = os.path.realpath(filename)
-      if fnmatch.fnmatch(filename, pattern):
+      if match(filename, pattern):
         matches.append(filename)
-  
+
   if single:
     return random.choice(matches)
   return matches
+
+def match(filename, pattern):
+  for i in pattern:
+    if i[0] == '-' and i[1:].lower in filename.lower():
+      return False
+    elif i not in filename.lower():
+      return False
+  return True
+
+def extract(filename):
+  files = []
+  tmp_file = tempfile.mkdtemp()
+  with zipfile.ZipFile(filename, 'r') as zip_ref:
+    for i in zip_ref.namelist():
+      ext = '.' + i.rpartition('.')[2].lower()
+      if ext in ['.png', '.jpg', '.gif']:
+        zip_ref.extract(i, tmp_file)
+        files.append(os.path.join(tmp_file, i))
+  return files

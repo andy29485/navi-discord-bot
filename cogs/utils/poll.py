936afdfb6005562221443de6ea27315b8463c6d4
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2016-12-28 19:24:54 -0500

    removed useless whitespace

--- cogs/utils/poll.py
+++ cogs/utils/poll.py
@@ -14,10 +14,10 @@ class Poll:
     self.channel  = channel
     self.sleep    = sleep
     self.polls    = p
-    
+
     for opt in options:
       self.options[opt] = set()
-  
+
   def vote(self, user : discord.User, message):
     v = False
     for i in self.options:
@@ -26,7 +26,7 @@ class Poll:
         v = True
       elif user in self.options[i]:
         self.options[i].remove(user)
-  
+
   async def start(self):
     message = 'Poll stated: \"{}\"\n{}'.format(self.question,
                                                '\n'.join(self.options))
@@ -35,26 +35,25 @@ class Poll:
     await asyncio.sleep(self.sleep)
     if self.ongoing:
       await self.stop()
-  
+
   async def stop(self):
     self.ongoing = False
     await self.bot.say(formatter.escape_mentions(self.results()))
     self.polls.pop(self.channel)
-  
+
   def results(self):
     out = ''
     formatting = '{{:<{}}} - {{:>{}}}\n'
     longest = [0, 0]
-    
+
     for i in self.options:
       if len(i) > longest[0]:
         longest[0] = len(i)
       if len(str(len(self.options[i]))) > longest[1]:
         longest[1] = len(str(len(self.options[i])))
-    
+
     formatting = formatting.format(*longest)
     for i in self.options:
       out += formatting.format(i, len(self.options[i]))
-      
-    return '**{}**:\n'.format(self.question) + formatter.code(out[:-1])
 
+    return '**{}**:\n'.format(self.question) + formatter.code(out[:-1])

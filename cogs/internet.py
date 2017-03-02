eba54036f0effb90b0b4da5f2fcfb3d6efb0ab10
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-03-02 12:29:11 -0500

    ddg and google both used

--- cogs/internet.py
+++ cogs/internet.py
@@ -12,13 +12,10 @@ class Search:
   def __init__(self, bot):
     self.bot = bot
 
-  @commands.command(name='duckduckgo', aliases=['ddg', 'd', 'g'])
+  @commands.command(name='search', aliases=['ddg', 'd', 's', 'g'])
   async def google(self, *, query):
     """
-    Searches DuckDuckGo and gives you top results.
-
-    Google had too many licencing issues
-    and the `g` alias is kept for convinece
+    Searches DuckDuckGo and Google and gives you top results.
     """
     try:
       entries = await self.get_search_entries(query)
@@ -35,11 +32,16 @@ class Search:
       await self.bot.say(msg)
 
   async def get_search_entries(self, query):
-    url = 'http://api.duckduckgo.com/'
-    params = {
+    url_d = 'http://api.duckduckgo.com/'
+    url_g = 'https://www.google.com/search'
+    params_d = {
       'q'      : query,
       'format' :'json',
       'pretty' : 0
+    }
+    params_g = {
+      'q': query,
+      'safe': 'off'
     }
     headers = {
       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0)'
@@ -49,7 +51,7 @@ class Search:
     entries = []
 
 
-    async with aiohttp.get(url, params=params, headers=headers) as resp:
+    async with aiohttp.get(url_d, params=params_d, headers=headers) as resp:
       if resp.status != 200:
         raise RuntimeError('DuckDuckGo somehow failed to respond.')
 
@@ -57,25 +59,47 @@ class Search:
 
       if results['Answer']:
         entries.append(results['Answer'])
-      for result in results['Results'] + results['RelatedTopics']:
-        try:
-          if 'Topics' in result:
-            for r in result['Topics']:
-              summary = html2text.html2text(r['Result']).replace('\n', ' ')
-              url     = r['FirstURL']
-              summery = re.sub(r'^\[[^\]]*\]\([^\)]*\) \\*- ', '', summery)
 
-              # if I ever cared about the description, this is how
-              entries.append('<{}>\n{}\n'.format(url, summary))
-          else:
-            summary = html2text.html2text(result['Result']).replace('\n', ' ')
-            url     = result['FirstURL']
-            summery = re.sub(r'^\[[^\]]*\]\([^\)]*\) \\*- ', '', summery)
+    async with aiohttp.get(url_g, params=params_g, headers=headers) as resp:
+      if resp.status != 200:
+        raise RuntimeError('Google somehow failed to respond.')
 
-            # if I ever cared about the description, this is how
-            entries.append('<{}>\n{}\n'.format(url, summary))
-        except:
-          pass
+      root = etree.fromstring(await resp.text(), etree.HTMLParser())
+
+      """
+      Tree looks like this.. sort of..
+      <div class="g">
+          ...
+          <h3>
+              <a href="/url?q=<url>" ...>title</a>
+          </h3>
+          ...
+          <span class="st">
+              <span class="f">date here</span>
+              summary here, can contain <em>tag</em>
+          </span>
+      </div>
+      """
+
+      search_nodes = root.findall(".//div[@class='g']")
+      for node in search_nodes:
+        entry_node = node.find(".//span[@class='st']")
+        if entry_node is None or not entry_node.text:
+          continue
+
+        url_node = node.find('.//h3/a')
+        if url_node is None:
+          continue
+
+        url = url_node.attrib['href']
+        if not url.startswith('/url?'):
+          continue
+
+        summary = html2text.html2text(etree.tostring(entry_node).decode('utf-8'))
+        url     = parse_qs(url[5:])['q'][0]
+
+        # if I ever cared about the description, this is how
+        entries.append('<{}>\n{}\n'.format(url, summary))
     return entries
 
 def setup(bot):

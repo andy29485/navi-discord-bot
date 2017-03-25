34d42bb9d7e02a3c0c9929436af38440aa894709
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-03-25 13:44:28 -0400

    Emby info bug fixed

--- cogs/emby.py
+++ cogs/emby.py
@@ -14,7 +14,7 @@ colours = [0x1f8b4c, 0xc27c0e, 0x3498db, 0x206694, 0x9b59b6,
            0x71368a, 0xe91e63, 0xe67e22, 0xf1c40f, 0x1abc9c,
            0x2ecc71, 0xa84300, 0xe74c3c, 0xad1457, 0x11806a]
 
-class E:
+class Emby:
   def __init__(self, bot):
     self.bot  = bot
     self.conf = Config('configs/emby.json')
@@ -27,11 +27,10 @@ class E:
       await self.bot.say(formatter.error("Please specify valid subcommand"))
 
   @emby.command(name='lookup', aliases=['info', 'i'], pass_context=True)
-  async def _info(self, ctx, *, *item_ids):
+  async def _info(self, ctx, *, item_ids = ''):
     """print emby server info, or an embed for each item id"""
-
     loop = asyncio.get_event_loop()
-    for item_id in item_ids:
+    for item_id in item_ids.split():
       item = await loop.run_in_executor(None, self.conn.info, item_id)
       em   = await loop.run_in_executor(None, makeEmbed, item)
       await self.bot.send_message(ctx.message.channel, embed=em)

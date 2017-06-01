63feca1e284f34b4b774802a836fdd24934de842
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-06-01 19:39:57 -0400

    reminder at should work now...

--- cogs/utils/reminders.py
+++ cogs/utils/reminders.py
@@ -6,6 +6,15 @@ import cogs.utils.heap as heap
 from dateutil import parser
 
 class Reminder:
+  tm = [re.compile(r'([T _-]*(?P<hour>\d\d?):(?P<min>\d\d)'+
+                  r'(:(?P<sec>\d\d))?(\s+(?P<meridiem>AM|PM))?)?'
+        )
+       ]
+  dt = [re.compile(r'(?P<year>\d{4})-(?P<month>\d\d)-(?P<day>\d\d)'),
+        re.compile(r'(?P<month>\d{1,2})[/\.-](?P<day>\d{1,2})'+
+                   r'[/\.-](?P<year>\d\d(\d\d)?)'
+        )
+       ]
   def __init__(self, channel_id, user_id, message, end_time=0):
     self.channel_id = channel_id
     self.user_id    = user_id
@@ -39,13 +48,46 @@ class Reminder:
          '(?i)(\\d+)\\s*w(eeks?)?'      : 604800,
          '(?i)(\\d+)\\s*months?'        : 2628000
     }
+    m_time = None
+    m_date = None
     if re.search(r'(?i)^(me)?\s*in', self.message):
-      offset = parser.parse(self.message, fuzzy=True).timestamp()
-    for t in times:
-      match = re.search(t, self.message)
-      self.message = re.sub(t, '', self.message).strip()
-      if match:
-        offset += times[t]*float(match.group(1))
+      date_time = datetime.datetime.today()
+      for t in Reminder.tm:
+        m_time = t.search(self.message)
+        if m_time:
+          self.message = self.message.replace(m_time.group(0), '')
+          if m_time.group('hour'):
+            h = int(m_time.group('hour'))
+            if m_time.group('meridiem').lower() == 'pm':
+              h += 12
+            date_time = date_time.replace(hour=h)
+          if m_time.group('min'):
+            m = int(m_time.group('min'))
+            date_time = date_time.replace(minute=m)
+          if m_time.group('sec'):
+            s = int(m_time.group('sec'))
+            date_time = date_time.replace(second=s)
+          break
+      for d in Reminder.dt:
+        m_date = d.search(self.message)
+        if m_date:
+          if m_date.group('year'):
+            y = int(m_date.group('year'))
+            date_time = date_time.replace(year=y)
+          if m_date.group('month'):
+            m = int(m_date.group('month'))
+            date_time = date_time.replace(month=m)
+          if m_date.group('day'):
+            d = int(m_date.group('day'))
+            date_time = date_time.replace(day=d)
+      if m_time or m_date:
+        offset = date_time.timestamp()
+    if not re.search(r'(?i)^(me)?\s*in',self.message) or not (m_date or m_time):
+      for t in times:
+        match = re.search(t, self.message)
+        self.message = re.sub(t, '', self.message).strip()
+        if match:
+          offset += times[t]*float(match.group(1))
     self.message = re.sub(r'(?i)^(me\s+)?(at|in)?\s*', '', self.message).strip()
     self.end_time = offset
 

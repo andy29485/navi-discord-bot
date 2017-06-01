8aceb58ae17997f7edb815a27155420b1bfa9372
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-05-31 20:50:47 -0400

    added a remind me feature

--- cogs/utils/reminders.py
+++ cogs/utils/reminders.py
@@ -3,6 +3,7 @@
 import re
 import time
 import cogs.utils.heap as heap
+from dateutil import parser
 
 class Reminder:
   def __init__(self, channel_id, user_id, message, end_time=0):
@@ -31,18 +32,21 @@ class Reminder:
   def parse_time(self):
     offset = time.time()
     times = {
-             '(?i)(\\d+)\\s*s(econds?)?'    : 1,
-             '(?i)(\\d+)\\s*m(in(ute)?s?)?' : 60,
-             '(?i)(\\d+)\\s*h(ours?)?'      : 3600,
-             '(?i)(\\d+)\\s*d(ays?)?'       : 86400,
-             '(?i)(\\d+)\\s*w(eeks?)?'      : 604800,
-             '(?i)(\\d+)\\s*months?'        : 2628000
+         '(?i)(\\d+)\\s*s(econds?)?'    : 1,
+         '(?i)(\\d+)\\s*m(in(ute)?s?)?' : 60,
+         '(?i)(\\d+)\\s*h(ours?)?'      : 3600,
+         '(?i)(\\d+)\\s*d(ays?)?'       : 86400,
+         '(?i)(\\d+)\\s*w(eeks?)?'      : 604800,
+         '(?i)(\\d+)\\s*months?'        : 2628000
     }
+    if re.search(r'(?i)^(me)?\s*in', self.message):
+      offset = parser.parse(self.message).timestamp()
     for t in times:
       match = re.search(t, self.message)
       self.message = re.sub(t, '', self.message).strip()
       if match:
         offset += times[t]*float(match.group(1))
+    self.message = re.sub(r'(?i)^(me\s+)?(at|in)?\s*', '', self.message).strip()
     self.end_time = offset
 
   def to_dict(self):

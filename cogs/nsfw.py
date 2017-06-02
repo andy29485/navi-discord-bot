92392d2328d71cf695638d315cfe4f04e3c6b80a
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-06-01 23:53:42 -0400

    fixed?

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -5,31 +5,38 @@ import asyncio
 import pybooru
 from discord import Embed
 from discord.ext import commands
+from cogs.utils.config import Config
 
 class Nsfw:
   def __init__(self, bot):
-    self.bot   = bot
-    self.yandere = pybooru.Moebooru('yandere')
+    self.bot      = bot
+    self.conf     = Config('configs/nsfw.json')
+    if 'yandere-conf' not in self.conf:
+      self.conf['yandere-conf']  = {}
+    if 'danbooru-conf' not in self.conf:
+      self.conf['danbooru-conf'] = {}
+    self.yandere  = pybooru.Moebooru('yandere',  **self.conf['yandere-conf'])
+    self.danbooru = pybooru.Danbooru('danbooru', **self.conf['danbooru-conf'])
 
-  @commands.command()
-  async def yandere(self, search_tags : str):
-    tags = re.split(',?\\s+', search_tags)
-    post = self.yandere.post_list(limit=1,tags=tags,random=True)
-    em   = Embed()
+  @commands.command(name='danbooru', aliases=['db'])
+  async def _danbooru(self, search_tags : str):
+    tags  = re.split(',?\\s+', search_tags)
+    posts = self.danbooru.post_list(limit=1,tags=tags,random=True)
+    em    = Embed()
 
-    if not post:
+    if not posts:
       await self.bot.say('could not find anything')
       return
 
-    post = post[0]
+    post = posts[0]
 
     em.title = search_tags
-    em.url   = 'https://yande.re/post/show/{}'.format(post['id'])
-    em.set_image(post['file_url'])
-    if post['tags']:
-      em.set_footer(text=post['tags'])
+    em.url   = 'https://danbooru.donmai.us/posts/{}'.format(post['id'])
+    em.set_image(url=post['large_file_url'])
+    if post['tag_string']:
+      em.set_footer(text=post['tag_string'])
 
-    self.bot.say(embed=em)
+    await self.bot.say(embed=em)
 
 
 def setup(bot):

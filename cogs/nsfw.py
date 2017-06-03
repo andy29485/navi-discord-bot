bf5f17edf379691c3e2ae36546eed24b68685122
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-06-02 20:16:59 -0400

    safe stuff added

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -13,12 +13,22 @@ class NSFW:
   def __init__(self, bot):
     self.bot      = bot
     self.conf     = Config('configs/nsfw.json')
+
+    if 'update' not in self.conf:
+      self.conf['update'] = {'safebooru':{'url':'https://safebooru.donmai.us'}}
+      self.conf.save()
+    pybooru.resources.SITE_LIST.update(self.conf['update'])
+
     if 'yandere-conf' not in self.conf:
       self.conf['yandere-conf']  = {}
     if 'danbooru-conf' not in self.conf:
       self.conf['danbooru-conf'] = {}
-    self.yandere  = pybooru.Moebooru('yandere',  **self.conf['yandere-conf'])
-    self.danbooru = pybooru.Danbooru('danbooru', **self.conf['danbooru-conf'])
+    if 'safebooru-conf' not in self.conf:
+      self.conf['safebooru-conf'] = {}
+
+    self.yandere  = pybooru.Moebooru('yandere',   **self.conf['yandere-conf'])
+    self.danbooru = pybooru.Danbooru('danbooru',  **self.conf['danbooru-conf'])
+    self.danbooru = pybooru.Danbooru('safebooru', **self.conf['safebooru-conf'])
 
   @commands.group(pass_context=True)
   async def nsfw(self, ctx):
@@ -95,6 +105,43 @@ class NSFW:
     em.set_image(url=u)
     if post['tags']:
       em.set_footer(text=post['tags'])
+
+    await self.bot.say(embed=em)
+
+  @nsfw.command(name='safebooru', aliases=['s', 'safe'])
+  async def _safebooru(self, search_tags : str):
+    """
+      searches safebooru for an image
+
+      usage: .nsfw safebooru tags1 tag2, tag_3, etc...
+      must specify at least 1 tag
+      will should not return nsfw images
+    """
+    tags  = re.split(',?\\s+', search_tags)
+    posts = self.safebooru.post_list(limit=1,tags=tags,random=True)
+    em    = Embed()
+
+    if not posts:
+      await self.bot.say('could not find anything')
+      return
+
+    post = posts[0]
+
+    em.title = search_tags
+    em.url   = 'https://safebooru.donmai.us/posts/{}'.format(post['id'])
+    u        = 'https://safebooru.donmai.us'
+    if 'large_file_url' in post:
+      u += post['large_file_url']
+    elif 'file_url' in post:
+      u += post['file_url']
+    else:
+      await self.bot.say('''
+              Sorry, there seems to be a premium tag in the query,
+              send me $20 if you you want to search it.
+      ''')
+    em.set_image(url=u)
+    if post['tag_string']:
+      em.set_footer(text=post['tag_string'])
 
     await self.bot.say(embed=em)
 

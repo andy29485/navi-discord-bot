0de8deba4b75476e9bff55da33fc478f4b7cb9b2
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-07-06 17:14:22 -0400

    puush optimizes by file hashes now

--- cogs/utils/puush.py
+++ cogs/utils/puush.py
@@ -4,6 +4,7 @@ from cogs.utils import find as azfind
 from cogs.utils.config import Config
 import requests
 import tempfile
+import hashlib
 
 conf = Config('configs/az.json')
 if 'path' not in conf:
@@ -20,17 +21,12 @@ if 'images' not in conf or type(conf['images']) != dict:
 account = puush.Account(conf['key'])
 
 def upload(paths, p=None):
-  tempfiles = []
+  if not paths:
+    return []
   if not p:
-    p = paths[0]
+    p = get_hash(paths[0])
   urls = ''
   for path in paths:
-    if path.startswith('http'):
-      t   = tempfile.NamedTemporaryFile(suffix=".jpg")
-      response = requests.get(path, stream=True, verify=False)
-      t.write(response.raw.read())
-      path = t.name
-      tempfiles.append(t)
     for i in range(3):
       try:
         image = account.upload(path)
@@ -43,15 +39,25 @@ def upload(paths, p=None):
       return 'could not upload image'
   conf['images'][p] = {'url':urls}
   conf.save()
-  for t in tempfiles:
-    t.close()
   return urls
 
 def get_url(path):
-  if path in conf['images']:
-    urls = conf['images'][path]['url']
+  tempfiles = []
+  if path.startswith('http'):
+    t   = tempfile.NamedTemporaryFile(suffix=".jpg")
+    response = requests.get(path, stream=True, verify=False)
+    t.write(response.raw.read())
+    path = t.name
+    tempfiles.append(t)
+
+  str_hash = get_hash(path)
+
+  if str_hash in conf['images']:
+    urls = conf['images'][str_hash]['url']
     try:
       if confirm_img(urls):
+        for t in tempfiles:
+          t.close()
         return urls
     except:
       pass
@@ -60,18 +66,28 @@ def get_url(path):
   if path.rpartition('.')[2].lower() in ['zip', 'cbz']:
     files = azfind.extract(path)
     if files:
-      out = upload(files, path)
+      out = upload(files, str_hash)
       for f in files:
         os.remove(f)
       os.rmdir(os.path.dirname(files[0]))
     else:
       out = 'archive found... but empty'
-    return out
   else:
-    return upload([path])
+    out = upload([path], str_hash)
+
+  for t in tempfiles:
+    t.close()
+  return out
 
 def confirm_img(urls):
   for url in urls.split('\n'):
     if url and requests.get(url, timeout=2).status_code != 200:
       return False
   return True
+
+def get_hash(path):
+  hasher = hashlib.md5()
+  with open(path, 'rb') as f:
+    buf = f.read()
+    hasher.update(buf)
+  return hasher.hexdigest()
